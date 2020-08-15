f11d244e053f816e89781a4bfdd37aad10f9fd09
GauravMehta10 <gaurav1058.cse18@chitkara.edu.in>
2020-08-15 20:56:26 +0530

    Code to print the tree's postorder transval in a single line using java.

---
+++ TreePostorderTraversal.java
@@ -0,0 +1,14 @@
+ public static void postOrder(Node root) {
+
+if(root == null)
+    {
+    return;
+}
+if(root!=null)
+    {
+     postOrder(root.left);
+     postOrder(root.right);
+     System.out.print(root.data+" ");
+
+}
+    }
