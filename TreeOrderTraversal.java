e48ce46fdc1f8a0368cdbf50951fc39481c00568
GauravMehta10 <gaurav1058.cse18@chitkara.edu.in>
2020-08-18 17:43:08 +0530

    Code to print level order traversal of a Binary tree in java.

---
+++ TreeOrderTraversal.java
@@ -0,0 +1,14 @@
+public static void levelOrder(Node root) {
+       Queue<Node> queue=new LinkedList<>();
+    queue.add(root);
+
+    while(!queue.isEmpty()) {
+        Node tempNode=queue.poll();
+        System.out.print(tempNode.data+" ");
+        if(tempNode.left!=null)
+            queue.add(tempNode.left);
+        if(tempNode.right!=null)
+            queue.add(tempNode.right);
+    }
+      
+    }
